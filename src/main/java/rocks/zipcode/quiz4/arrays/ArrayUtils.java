08e6a877411aa9720a3e839e4fe5885497785489
chipfody <60615532+chipfody@users.noreply.github.com>
2020-03-20 17:06:27 -0400

    save1

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,30 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        Integer middleElement = (values.length / 2);
+        return rebuildArray(values, middleElement);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
         return null;
+    }
+
+    public static String[] rebuildArray(String[] newArray, Integer remove) {
+        String[] output = new String[newArray.length - 1];
+        for (int i = 0; i < remove; i++) {
+            for (int j = remove; j < newArray.length - 1; j++) {
+                output[j] = newArray[j + 1];
+            }
+        }
+        return output;
+
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,14 +1,23 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length() / 2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
+//        if (str.length() == 1) {
+//            return str.toUpperCase();
+//        }
+//        int indexToCapitalize = str.indexOf(getMiddleCharacter(str));
+//        System.out.println(indexToCapitalize);
+//
+//        return str.substring(0, indexToCapitalize) + str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase() + str.substring(indexToCapitalize + 1);
         return null;
     }
 
@@ -17,11 +26,29 @@ public class StringUtils {
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        char array[] = str.toCharArray();
+        Boolean Isogram = false;
+        Arrays.sort(array);
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (array[i] == array[i + 1]) {
+                Isogram = false;
+                break;
+            } else {
+                Isogram = true;
+            }
+        }  return Isogram;
+
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char array[] = str.toCharArray();
+        Boolean consecutive = false;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (array[i] == array[i + 1]) {
+                consecutive = true;
+            }
+        }
+        return consecutive;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
